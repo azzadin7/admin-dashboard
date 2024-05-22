1302b9ca0947e2f78d3948a7a0d6dfe3a257de74
azzadin7 <omegaelfaita@gmail.com>
2024-05-22 11:38:26 +0700

    Test Update Setting

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,6 +3,8 @@ export default async function SettingsPage() {
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
       <div className="flex items-center">
         <h1 className="font-semibold text-lg md:text-2xl">Settings</h1>
+      </div>
+      <div>
         <h2>Halo, disini adalah halaman setting</h2>
       </div>
     </main>
